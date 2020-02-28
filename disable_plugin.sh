3389e431c865610d3bbb87192495fdef74ccabca
hui.zhao <hui.zhao@ctrip.com>
2020-02-28 14:58:20 +0800

    #Changed# 判断操作系统

--- disable_plugin.sh
+++ disable_plugin.sh
@@ -1,3 +1,12 @@
 #!/usr/bin
 
-sed -i "" "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=true#g" gradle.properties
+if [[ `uname` == 'Darwin' ]]; then
+    sed -i "" "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=true#g" gradle.properties
+fi
+
+if [[ `uname` == 'Linux' ]]; then
+    sed -i "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=true#g" gradle.properties
+fi
+
+
+
--- enable_plugin.sh
+++ enable_plugin.sh
@@ -1,3 +1,10 @@
 #!/usr/bin
 
-sed -i "" "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=false#g" gradle.properties
+if [[ `uname` == 'Darwin' ]]; then
+    sed -i "" "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=false#g" gradle.properties
+fi
+
+if [[ `uname` == 'Linux' ]]; then
+    sed -i "s#^DISABLE_PLUGIN=.*#DISABLE_PLUGIN=false#g" gradle.properties
+fi
+
