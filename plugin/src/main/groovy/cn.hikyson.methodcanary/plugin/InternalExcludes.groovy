bef60cb2e99f24eb8209de707ed9c4fa0b9f6cc7
hui.zhao <hui.zhao@ctrip.com>
2020-02-29 13:13:51 +0800

    #Changed# 默认排除godeye的插桩代码

--- plugin/src/main/groovy/cn.hikyson.methodcanary/plugin/InternalExcludes.groovy
+++ plugin/src/main/groovy/cn.hikyson.methodcanary/plugin/InternalExcludes.groovy
@@ -3,6 +3,6 @@ package cn.hikyson.methodcanary.plugin
 class InternalExcludes {
 
     boolean isMethodExclude(ClassInfo classInfo, MethodInfo methodInfo) {
-        return classInfo.name.startsWith('cn/hikyson/methodcanary/lib/')
+        return classInfo.name.startsWith('cn/hikyson/methodcanary/lib/') || classInfo.name.startsWith('cn/hikyson/godeye/core/') || classInfo.name.startsWith('cn/hikyson/godeye/monitor/') || classInfo.name.startsWith('cn/hikyson/android/godeye/')
     }
 }
